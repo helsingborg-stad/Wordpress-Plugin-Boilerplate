5a7eb3e2ba80cfc8eba4871985991840b09062ab
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-01-31 01:07:17 +0100

    Added more variables

--- plugin-name.php
+++ plugin-name.php
@@ -2,11 +2,11 @@
 
 /**
  * Plugin Name:       (#plugin_name#)
- * Plugin URI:        http://example.com
- * Description:       A short plugin description
+ * Plugin URI:        (#plugin_url#)
+ * Description:       (#plugin_description#)
  * Version:           1.0.0
- * Author:            Kristoffer Svanmark
- * Author URI:        http://www.svanmark.se
+ * Author:            (#plugin_author#)
+ * Author URI:        (#plugin_author_url#)
  * License:           MIT
  * License URI:       https://opensource.org/licenses/MIT
  * Text Domain:       @sh{#plugin_slug#}
@@ -22,10 +22,10 @@ define('(#plugin_cap#)_PATH', plugin_dir_path(__FILE__));
 define('(#plugin_cap#)_URL', plugins_url('', __FILE__));
 define('(#plugin_cap#)_TEMPLATE_PATH', (#plugin_cap#)_PATH . 'templates/');
 
-load_plugin_textdomain('modularity', false, plugin_basename(dirname(__FILE__)) . '/languages');
+load_plugin_textdomain('(#plugin_slug#)', false, plugin_basename(dirname(__FILE__)) . '/languages');
 
 require_once (#plugin_cap#)_PATH . 'source/php/Vendor/Psr4ClassLoader.php';
-require_once (#plugin_cap#) . 'Public.php';
+require_once (#plugin_cap#)_PATH . 'Public.php';
 
 // Instantiate and register the autoloader
 $loader = new (#plugin_namespace#)\Vendor\Psr4ClassLoader();
