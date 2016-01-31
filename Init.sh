c8f5ad9b47123082fee9ff43bdcea6695641a3d1
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-01-31 01:07:07 +0100

    Updated installer script

--- Init.sh
+++ Init.sh
@@ -5,6 +5,14 @@ echo "\033[34m\033[1mEnter the name of your plugin (readable):\033[0m "
 read plugin_name
 find ./ -type f ! -name "Init.sh" -exec sed -i '' -e "s/(#plugin_name#)/$plugin_name/g" {} >/dev/null 2>&1 \;
 
+echo "\033[34m\033[1mEnter short plugin description:\033[0m "
+read plugin_description
+find ./ -type f ! -name "Init.sh" -exec sed -i '' -e "s/(#plugin_description#)/$plugin_description/g" {} >/dev/null 2>&1 \;
+
+echo "\033[34m\033[1mEnter plugin url:\033[0m "
+read plugin_url
+find ./ -type f ! -name "Init.sh" -exec sed -i '' -e "s/(#plugin_url#)/$plugin_url/g" {} >/dev/null 2>&1 \;
+
 echo "\033[34m\033[1mEnter plugin slug:\033[0m "
 read plugin_slug
 find ./ -type f ! -name "Init.sh" -exec sed -i '' -e "s/(#plugin_slug#)/$plugin_slug/g" {} >/dev/null 2>&1 \;
@@ -17,10 +25,21 @@ echo "\033[34m\033[1mEnter plugin namespace:\033[0m "
 read plugin_namespace
 find ./ -type f ! -name "Init.sh" -exec sed -i '' -e "s/(#plugin_namespace#)/$plugin_namespace/g" {} >/dev/null 2>&1 \;
 
+echo "\033[34m\033[1mEnter plugin author name:\033[0m "
+read plugin_author
+find ./ -type f ! -name "Init.sh" -exec sed -i '' -e "s/(#plugin_author#)/$plugin_author/g" {} >/dev/null 2>&1 \;
+
+echo "\033[34m\033[1mEnter plugin author url:\033[0m "
+read plugin_author_url
+find ./ -type f ! -name "Init.sh" -exec sed -i '' -e "s/(#plugin_author_url#)/$plugin_author_url/g" {} >/dev/null 2>&1 \;
+
 # Rename plugin-name.* to plugin_slug
 mv ./plugin-name.php ${plugin_slug}.php
 mv ./source/js/plugin-name.js ./source/js/${plugin_slug}.js
 mv ./source/sass/plugin-name.scss ./source/sass/${plugin_slug}.scss
+
+rm README.md
+mv ./README_template.md README.md
 
 echo "\033[34m\033[1mLean back while I install required node modules for you…\033[0m"
 npm install
