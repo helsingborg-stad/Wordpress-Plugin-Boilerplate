8d3882aeae6167963dabc8702d84244773757cc1
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-01-31 18:00:03 +0100

    Add enqueue functions

--- source/php/App.php
+++ source/php/App.php
@@ -6,6 +6,25 @@ class App
 {
     public function __construct()
     {
-        // Constructor
+        add_action('admin_enqueue_scripts', array($this, 'enqueueStyles'));
+        add_action('admin_enqueue_scripts', array($this, 'enqueueScripts'));
+    }
+
+    /**
+     * Enqueue required style
+     * @return void
+     */
+    public function enqueueStyles()
+    {
+
+    }
+
+    /**
+     * Enqueue required scripts
+     * @return void
+     */
+    public function enqueueScripts()
+    {
+
     }
 }
